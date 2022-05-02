da6717e80713be3069a22df9aa7900aeef94d80b
Alejandro Otero <locolajefa@gmail.com>
2022-05-02 20:38:36 -0300

    /productos terminado

--- GrupoArchivo.js
+++ GrupoArchivo.js
@@ -1,15 +1,9 @@
 const fs = require('fs')
-const Producto = require('./Producto.js')
 
 class GrupoArchivo {
     constructor(ruta) {
         this.ruta = ruta
         this.productos = []
-    }
-
-    _guardar() {
-        const productosComoJson = JSON.stringify(this.productos, null, 2)
-        return fs.promises.writeFile(this.ruta, productosComoJson)
     }
 
     _leer() {
@@ -20,35 +14,11 @@ class GrupoArchivo {
             })
     }
 
-    estaIncluido(id) {
-        if(this.productos.length == 0) {
-            return false
-        }
-        return this.productos.some(prod => prod.id === id)
-    }
-
-    async agregar(datos) {
-        const producto = new Producto(datos.title, datos.price, datos.thumbnail, datos.id)
-        await this._leer()
-        if (!this.estaIncluido(datos.id)) {
-            this.productos.push(producto)
-            await this._guardar()
-        }
-    }
-
     async obtenerTodas() {
         await this._leer()
         return [...this.productos]
     }
 
-    async borrarProducto(id) {
-        await this._leer()
-        const indice = this.productos.findIndex(producto => producto.id === id)
-        if (indice !== -1) {
-            this.productos.splice(indice, 1)
-            await this._guardar()
-        }
-    }
 }
 
 module.exports = GrupoArchivo
--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require("express");
 const GrupoArchivo = require('./GrupoArchivo.js')
 
 const path = require("path");
-
 const app = express();
 
-async function main() {
-    const grupo = new GrupoArchivo('./productos.json')
-
-    console.log(await grupo.obtenerTodas())
-}
-
-main()
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname + "/index.html"))
 })
+
+const grupo = new GrupoArchivo('./productos.json')
+
+app.get("/productos", async (req, res) => {
+    try {
+        res.json(await grupo.obtenerTodas());
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
 app.listen(8080, ()=>{
     console.log("server listening on port", 8080)
