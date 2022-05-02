0f639ef2e09f49f2ef31d696e83f081340e27c6a
Alejandro Otero <locolajefa@gmail.com>
2022-05-02 17:55:33 -0300

    base

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ const app = express();
 async function main() {
     const grupo = new GrupoArchivo('./productos.json')
 
-    const producto1 = {
-        title: 'escuadra',
-        price: 123.45,
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-        id: 1
-    }
-
-    await grupo.agregar(producto1)
-
     console.log(await grupo.obtenerTodas())
 }
 
