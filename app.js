9a6592bf735f80a2ad7e2a1bc684c5f86c6b4fe0
Alejandro Otero <locolajefa@gmail.com>
2022-05-03 17:52:58 -0300

    /productosRandom

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require("express");
-const GrupoArchivo = require('./GrupoArchivo.js')
+const ArchiveGroup = require('./ArchiveGroup.js')
+const fs = require("fs")
 
 const path = require("path");
 const app = express();
 
+const group = new ArchiveGroup('./productos.json')
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-const grupo = new GrupoArchivo('./productos.json')
+function productsNormal() {
+    app.get("/productos", async (req, res) => {
+        try {
+            res.json(await group.getAll());
+        } catch(e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
+    });
+}
 
-app.get("/productos", async (req, res) => {
-    try {
-        res.json(await grupo.obtenerTodas());
-    } catch(e) {
-        console.log(e);
-        res.sendStatus(500);
-    }
-});
+function productsRandom() {
+    let rawData = fs.readFileSync('productos.json');
+    let productsRandom = JSON.parse(rawData);
+    let randomNumber = Math.floor(Math.random() * productsRandom.length);
+    
+    app.get("/productosRandom", (req, res) => {
+        try {
+            res.json(productsRandom[randomNumber]);
+        } catch(e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
+    });
+}
 
 app.listen(8080, ()=>{
     console.log("server listening on port", 8080)
 })
+
+productsNormal()
+productsRandom()
