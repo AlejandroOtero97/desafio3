9b9d0c735200c772313529ce4cef9edfadcef57b
Alejandro Otero <locolajefa@gmail.com>
2022-05-09 17:08:57 -0300

    folder delete

--- desafio5/js/inicio.js
+++
@@ -1,9 +0,0 @@
-async function main(){
-    const response = await fetch('template/inicio.handlebars')
-    const templateText = await response.text()
-    const templateFn = Handlebars.compile(templateText);
-    const html = templateFn({nombre:'coder'});
-    document.getElementById('espacioParaContenido').innerHTML = html;
-}
-
-
